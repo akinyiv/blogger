1b9e1502c3986962b45898d1fa269e6ba38fdabe
akinyiv <akinyivee10@gmail.com>
2023-04-09 18:26:17 +0300

    task

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -1,11 +1,13 @@
 defmodule Blogger do
    require HTTPoison
 
+# Fetch a blog post from Medium
   def fetch_blog do
     HTTPoison.get!("https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91")
 
   end
 
+# Converts a list of strings to uppercase
   def uppercase_all(list) do
     Enum.map(list, &String.upcase/1)
 
