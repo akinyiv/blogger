b27a6e748ec18854ba65943f2afe38fe953eecaf
akinyiv <akinyivee10@gmail.com>
2023-04-10 22:39:55 +0300

    mix test

--- test/blogger_test.exs
+++ test/blogger_test.exs
@@ -2,6 +2,10 @@ defmodule BloggerTest do
   use ExUnit.Case
   doctest Blogger
 
+  test "fetch_blog fetches a blog post from URL" do
+    assert Blogger.fetch_blog.status_code == 200
+  end
+
   test "uppercase_all converts all elements to uppercase" do
     assert Blogger.uppercase_all(["cOw", "goat", "sheep"]) == ["COW", "GOAT", "SHEEP"]
   end
