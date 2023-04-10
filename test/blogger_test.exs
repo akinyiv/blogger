9dbe862e137b2a84c9f1f26e14020cd4ff920f62
akinyiv <akinyivee10@gmail.com>
2023-04-10 22:17:33 +0300

    mix test

--- test/blogger_test.exs
+++ test/blogger_test.exs
@@ -2,7 +2,7 @@ defmodule BloggerTest do
   use ExUnit.Case
   doctest Blogger
 
-  test "greets the world" do
-    assert Blogger.hello() == :world
+  test "uppercase_all converts all elements to uppercase" do
+    assert Blogger.uppercase_all(["cOw", "goat", "sheep"]) == ["COW", "GOAT", "SHEEP"]
   end
 end
